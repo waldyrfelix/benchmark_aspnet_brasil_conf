ff786c88e66a060a62e7960448848198b905111f
Waldyr Felix <waldyrfelix@gmail.com>
2016-06-01 19:26:26 -0300

    k

--- aspnetcore-benchmark/Startup.cs
+++ aspnetcore-benchmark/Startup.cs
@@ -16,8 +16,6 @@ namespace aspnetcore_benchmark
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-              //``  .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
         }
