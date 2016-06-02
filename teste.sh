6d7e3ee7bccf2294d51eeda07acd070cfd640ad1
Waldyr Felix <waldyrfelix@gmail.com>
2016-06-01 23:58:51 -0300

    tese

--- teste.sh
+++ teste.sh
@@ -2,7 +2,7 @@
 
 HOST=localhost
 
-echo "Inicio do teste FILE"
+echo "Inicio do teste FILE" >> log.txt
 echo "16 conexões" >> log.txt
 wrk -t 16 -c 16 -d 10s http://$HOST:5000/api/benchmark/file >> log.txt
 
@@ -25,3 +25,27 @@ wrk -t 32 -c 256 -d 10s http://$HOST:5000/api/benchmark/file >> log.txt
 echo "------------" >> log.txt
 echo "512 conexões" >> log.txt
 wrk -t 32 -c 512 -d 10s http://$HOST:5000/api/benchmark/file >> log.txt
+
+echo "Inicio do teste List" >> log.txt
+echo "16 conexões" >> log.txt
+wrk -t 16 -c 16 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
+
+echo "-----------" >> log.txt
+echo "32 conexões" >> log.txt
+wrk -t 32 -c 32 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
+
+echo "-----------" >> log.txt
+echo "64 conexões" >> log.txt
+wrk -t 32 -c 64 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
+
+echo "------------" >> log.txt
+echo "128 conexões" >> log.txt
+wrk -t 32 -c 128 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
+
+echo "------------" >> log.txt
+echo "256 conexões" >> log.txt
+wrk -t 32 -c 256 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
+
+echo "------------" >> log.txt
+echo "512 conexões" >> log.txt
+wrk -t 32 -c 512 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
