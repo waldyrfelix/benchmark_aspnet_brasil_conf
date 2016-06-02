88d162206dc73e5d757a34c352d6fba3c20b4834
Waldyr Felix <waldyrfelix@gmail.com>
2016-06-02 00:58:12 -0300

    chefe é chefe

--- teste.sh
+++ teste.sh
@@ -1,7 +1,45 @@
 #!/bin/bash
 
-HOST=localhost
+HOST=10.0.0.4
 
+
+echo "====================" >> log.txt
+echo "Inicio do teste List" >> log.txt
+echo "16 conexões" >> log.txt
+wrk -t 16 -c 16 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
+
+echo "-----------" >> log.txt
+echo "32 conexões" >> log.txt
+wrk -t 32 -c 32 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
+
+echo "-----------" >> log.txt
+echo "64 conexões" >> log.txt
+wrk -t 32 -c 64 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
+
+echo "------------" >> log.txt
+echo "128 conexões" >> log.txt
+wrk -t 32 -c 128 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
+
+echo "------------" >> log.txt
+echo "256 conexões" >> log.txt
+wrk -t 32 -c 256 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
+
+echo "------------" >> log.txt
+echo "512 conexões" >> log.txt
+wrk -t 32 -c 512 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
+
+echo "------------" >> log.txt
+echo "1024 conexões" >> log.txt
+wrk -t 32 -c 1024 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
+
+echo "------------" >> log.txt
+echo "2048 conexões" >> log.txt
+wrk -t 32 -c 2048 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
+
+
+
+
+echo "====================" >> log.txt
 echo "Inicio do teste FILE" >> log.txt
 echo "16 conexões" >> log.txt
 wrk -t 16 -c 16 -d 10s http://$HOST:5000/api/benchmark/file >> log.txt
@@ -26,26 +64,10 @@ echo "------------" >> log.txt
 echo "512 conexões" >> log.txt
 wrk -t 32 -c 512 -d 10s http://$HOST:5000/api/benchmark/file >> log.txt
 
-echo "Inicio do teste List" >> log.txt
-echo "16 conexões" >> log.txt
-wrk -t 16 -c 16 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
-
-echo "-----------" >> log.txt
-echo "32 conexões" >> log.txt
-wrk -t 32 -c 32 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
-
-echo "-----------" >> log.txt
-echo "64 conexões" >> log.txt
-wrk -t 32 -c 64 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
+echo "------------" >> log.txt
+echo "1024 conexões" >> log.txt
+wrk -t 32 -c 1024 -d 10s http://$HOST:5000/api/benchmark/file >> log.txt
 
 echo "------------" >> log.txt
-echo "128 conexões" >> log.txt
-wrk -t 32 -c 128 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
-
-echo "------------" >> log.txt
-echo "256 conexões" >> log.txt
-wrk -t 32 -c 256 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
-
-echo "------------" >> log.txt
-echo "512 conexões" >> log.txt
-wrk -t 32 -c 512 -d 10s http://$HOST:5000/api/benchmark/list >> log.txt
+echo "2048 conexões" >> log.txt
+wrk -t 32 -c 2048 -d 10s http://$HOST:5000/api/benchmark/file >> log.txt
